1ec2874ed06ff5e29883f4a0e5dea70ab3017037
Tim Richardson <tim.richardson@genomicsengland.co.uk>
2025-09-05 20:51:50 +0100

    chore(apphosting): disable secret envs; make client envs optional for no-env build

--- src/env.js
+++ src/env.js
@@ -22,12 +22,12 @@ export const env = createEnv({
    * `NEXT_PUBLIC_`.
    */
   client: {
-    NEXT_PUBLIC_FIREBASE_API_KEY: z.string(),
-    NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN: z.string(),
-    NEXT_PUBLIC_FIREBASE_PROJECT_ID: z.string(),
-    NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET: z.string(),
-    NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID: z.string(),
-    NEXT_PUBLIC_FIREBASE_APP_ID: z.string(),
+    NEXT_PUBLIC_FIREBASE_API_KEY: z.string().optional(),
+    NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN: z.string().optional(),
+    NEXT_PUBLIC_FIREBASE_PROJECT_ID: z.string().optional(),
+    NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET: z.string().optional(),
+    NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID: z.string().optional(),
+    NEXT_PUBLIC_FIREBASE_APP_ID: z.string().optional(),
   },
 
   /**
